e8bf93b4fada856ccecaa40dbedbe2d36437034d
Fernando Ferrer Santos <fernando.ferrer@uegmobile.com>
2017-01-13 18:49:59 +0100

    Complete cell

---
+++ appcuisineObjC/MarsRoverTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MarsRoverTableViewCell.h
+//  appcuisineObjC
+//
+//  Created by Fernando Ferrer Santos on 13/1/17.
+//  Copyright © 2017 Fernando Ferrer Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MarsRoverTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *earthDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cameraFullNameLabel;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *actIndicatorView;
+@property (weak, nonatomic) IBOutlet UIImageView *cameraImageView;
+
+@end
---
+++ appcuisineObjC/MarsRoverTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MarsRoverTableViewCell.m
+//  appcuisineObjC
+//
+//  Created by Fernando Ferrer Santos on 13/1/17.
+//  Copyright © 2017 Fernando Ferrer Santos. All rights reserved.
+//
+
+#import "MarsRoverTableViewCell.h"
+
+@implementation MarsRoverTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
